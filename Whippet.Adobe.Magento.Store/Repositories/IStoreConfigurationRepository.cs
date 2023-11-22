a520e276cdf18b84e0f8ca4d03f294e10bdd2914
Adam Hardin <ahardin@superduperinc.com>
2023-11-22 11:33:23 -0500

    - Added repository interfaces for Store namespace - Added missing repository project for Taxes

---
+++ Whippet.Adobe.Magento.Store/Repositories/IStoreConfigurationRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Athi.Whippet.Data;
+using Athi.Whippet.Data.CQRS;
+using Athi.Whippet.Adobe.Magento.Data;
+
+namespace Athi.Whippet.Adobe.Magento.Store.Repositories
+{
+    /// <summary>
+    /// Represents a data repository for <see cref="StoreConfiguration"/> objects.
+    /// </summary>
+    public interface IStoreConfigurationRepository : IMagentoEntityRepository<StoreConfiguration>, IWhippetExternalQueryRepository<StoreConfiguration, uint>
+    {
+        /// <summary>
+        /// Retrieves the <see cref="StoreConfiguration"/> object with the specified <see cref="Store"/> code.
+        /// </summary>
+        /// <param name="code">Code of the <see cref="Store"/> to retrieve the configuration for.</param>
+        /// <returns><see cref="WhippetResultContainer{TEntity}"/> containing the result of the operation.</returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        WhippetResultContainer<StoreConfiguration> Get(string code);
+        
+        /// <summary>
+        /// Retrieves the <see cref="Store"/> object with the specified code.
+        /// </summary>
+        /// <param name="code">Code of the <see cref="Store"/> to retrieve.</param>
+        /// <param name="cancellationToken">Cancellation token.</param>
+        /// <returns><see cref="WhippetResultContainer{TEntity}"/> containing the result of the operation.</returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        Task<WhippetResultContainer<StoreConfiguration>> Get(string code, CancellationToken? cancellationToken = null);
+    }
+}
---
+++ Whippet.Adobe.Magento.Store/Repositories/IStoreRepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Athi.Whippet.Data;
+using Athi.Whippet.Data.CQRS;
+using Athi.Whippet.Adobe.Magento.Data;
+
+namespace Athi.Whippet.Adobe.Magento.Store.Repositories
+{
+    /// <summary>
+    /// Represents a data repository for <see cref="Store"/> objects.
+    /// </summary>
+    public interface IStoreRepository : IMagentoEntityRepository<Store>, IWhippetExternalQueryRepository<Store, uint>
+    {
+        /// <summary>
+        /// Retrieves the <see cref="Store"/> object with the specified code.
+        /// </summary>
+        /// <param name="name">Name of the <see cref="Store"/> to retrieve.</param>
+        /// <returns><see cref="WhippetResultContainer{TEntity}"/> containing the result of the operation.</returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        WhippetResultContainer<Store> Get(string name);
+        
+        /// <summary>
+        /// Retrieves the <see cref="Store"/> object with the specified name.
+        /// </summary>
+        /// <param name="name">Name of the <see cref="Store"/> to retrieve.</param>
+        /// <param name="cancellationToken">Cancellation token.</param>
+        /// <returns><see cref="WhippetResultContainer{TEntity}"/> containing the result of the operation.</returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        Task<WhippetResultContainer<Store>> GetAsync(string name, CancellationToken? cancellationToken = null);
+        
+        /// <summary>
+        /// Retrieves the <see cref="Store"/> object with the specified code.
+        /// </summary>
+        /// <param name="code">Code of the <see cref="Store"/> to retrieve.</param>
+        /// <returns><see cref="WhippetResultContainer{TEntity}"/> containing the result of the operation.</returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        WhippetResultContainer<Store> GetByCode(string code);
+        
+        /// <summary>
+        /// Retrieves the <see cref="Store"/> object with the specified code.
+        /// </summary>
+        /// <param name="code">Code of the <see cref="Store"/> to retrieve.</param>
+        /// <param name="cancellationToken">Cancellation token.</param>
+        /// <returns><see cref="WhippetResultContainer{TEntity}"/> containing the result of the operation.</returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        Task<WhippetResultContainer<Store>> GetByCodeAsync(string code, CancellationToken? cancellationToken = null);
+    }
+}
