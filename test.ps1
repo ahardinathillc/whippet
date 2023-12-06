b9872e202712ff7d5bd10e3ea13625b392701206
Adam Hardin <ahardin@superduperinc.com>
2023-12-06 10:39:57 -0500

    - main-1

--- test.ps1
+++
@@ -1,8 +0,0 @@
-$currentDirectory = $PWD.Path
-$destinationDirectory = "/Publish"
-
-$destinationDirectory = $currentDirectory + $destinationDirectory
-
-New-Item -ItemType Directory -Force -Path $destinationDirectory
-
-Copy-Item -Path $PWD.Path -Filter "*.dll" -Recurse -Destination $destinationDirectory -Container -Verbose
