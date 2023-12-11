6849975de8e148827244555721767e5c422ccf95
Adam Hardin <ahardin@superduperinc.com>
2023-12-11 16:47:34 -0500

    Importing digital library for SD

---
+++ Whippet.SuperDuper.DigitalLibrary.Legacy/LegacyDigitalLibraryAccount.cs
@@ -0,0 +1,10 @@
+using System;
+using Athi.Whippet.Data;
+
+namespace Athi.Whippet.SuperDuper.DigitalLibrary.Legacy
+{
+    public class LegacyDigitalLibraryAccount : WhippetEntity, IWhippetEntity, ILegacyDigitalLibraryAccount, 
+    {
+        
+    }
+}
