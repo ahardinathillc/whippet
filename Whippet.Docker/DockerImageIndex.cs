b9615508602e4d5df994a7888607249dcb86130b
Adam Hardin <adam.hardin@athi-llc.com>
2023-12-31 23:10:14 -0500

    Working on Docker unit test base class

---
+++ Whippet.Docker/DockerImageIndex.cs
@@ -0,0 +1,25 @@
+﻿namespace Athi.Whippet.Docker
+{
+    /// <summary>
+    /// Provides an index of available known Docker images used by Whippet. This class cannot be inherited.
+    /// </summary>
+    public static class DockerImageIndex
+    {
+        /// <summary>
+        /// Provides an index of available known Docker database images used by Whippet. This class cannot be inherited.
+        /// </summary>
+        public static class Databases
+        {
+            /// <summary>
+            /// Provides an index of available known Docker Microsoft database images used by Whippet. This class cannot be inherited.
+            /// </summary>
+            public static class Microsoft
+            {
+                /// <summary>
+                /// Microsoft SQL Server 2022.
+                /// </summary>
+                public const string MsSql2022 = "mcr.microsoft.com/mssql/server:2022-latest";
+            }
+        }
+    }
+}
---
+++ Whippet.Testing.Infrastructure/WhippetUnitTestBase.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using Docker.DotNet;
+using Docker.DotNet.Models;
+using Athi.Whippet.Docker;
+
+namespace Athi.Whippet.Testing.Infrastructure
+{
+    /// <summary>
+    /// Base class for all unit tests in Whippet. This class must be inherited.
+    /// </summary>
+    public abstract class WhippetUnitTestBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WhippetUnitTestBase"/> class with no arguments.
+        /// </summary>
+        protected WhippetUnitTestBase()
+        { }
+
+        /// <summary>
+        /// Creates a new <see cref="IDockerClient"/> with a Microsoft SQL Server database image preloaded.
+        /// </summary>
+        /// <param name="sqlImage">Microsoft SQL Server Docker image to use.</param>
+        /// <returns><see cref="IDockerClient"/> object.</returns>
+        protected virtual async Task<IDockerClient> CreateSqlServerImage(string sqlImage = DockerImageIndex.Databases.Microsoft.MsSql2022)
+        {
+            ImagesCreateParameters imageParameters = new ImagesCreateParameters();
+            imageParameters.FromImage = sqlImage;
+
+            return await CreateImage(imageParameters);
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="IDockerClient"/> with a Microsoft SQL Server database image already loaded in the client.
+        /// </summary>
+        /// <param name="sqlImage">Microsoft SQL Server Docker image to create a container from.</param>
+        /// <param name="createImage">If <see langword="true"/>, will load the image by calling <see cref="CreateSqlServerImage"/> prior to initialization; otherwise, will create a container based on the supplied image name.</param>
+        /// <returns><see cref="IDockerClient"/> object.</returns>
+        protected virtual async Task<IDockerClient> CreateSqlServerContainer(string sqlImage = DockerImageIndex.Databases.Microsoft.MsSql2022, bool createImage = true)
+        {
+            IDockerClient client = null;
+            bool failed = false;
+            
+            try
+            {
+                // https://github.com/dotnet/Docker.DotNet/
+            }
+            catch (Exception e)
+            {
+                failed = true;
+                throw;
+            }
+            finally
+            {
+                if (failed && (client != null))
+                {
+                    client.Dispose();
+                    client = null;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="IDockerClient"/> with a Microsoft SQL Server database image preloaded.
+        /// </summary>
+        /// <param name="imageParameters"><see cref="ImagesCreateParameters"/> object that contains the options used to load the image to create a container from.</param>
+        /// <param name="authConfig">Authorization options.</param>
+        /// <param name="progress">Progress reporter of the operation.</param>
+        /// <returns><see cref="IDockerClient"/> object with the configured image.</returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        protected virtual async Task<IDockerClient> CreateImage(ImagesCreateParameters imageParameters, AuthConfig authConfig = null, IProgress<JSONMessage> progress = null)
+        {
+            if (imageParameters == null)
+            {
+                throw new ArgumentNullException(nameof(imageParameters));
+            }
+            else
+            {
+                IDockerClient client = null;
+                bool failed = false;
+
+                try
+                {
+                    client = WhippetDockerClient.CreateLocalClient();
+                    await client.Images.CreateImageAsync(imageParameters, authConfig, progress ?? new Progress<JSONMessage>());
+                }
+                catch (Exception e)
+                {
+                    failed = true;
+                    throw;
+                }
+                finally
+                {
+                    if (failed)
+                    {
+                        if (client != null)
+                        {
+                            client.Dispose();
+                            client = null;
+                        }
+                    }
+                }
+
+                return client;
+            }
+        }
+
+        protected virtual async Task<IDockerClient> CreateContainer(CreateContainerParameters containerParameters)
+        {
+            
+        }
+    }
+}
